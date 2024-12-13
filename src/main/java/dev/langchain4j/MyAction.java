9675d1cefa7bc86dbb7e34da9441d112bec24eb9
LangChain4j <info@langchain4j.dev>
2024-12-13 15:17:33 +0100

    Init the project

---
+++ src/main/java/dev/langchain4j/MyAction.java
@@ -0,0 +1,14 @@
+package dev.langchain4j;
+
+import io.quarkiverse.githubaction.Action;
+import io.quarkiverse.githubaction.Commands;
+
+public class MyAction {
+
+    @Action
+    void action(Commands commands) {
+        commands.notice("Hello from Quarkus GitHub Action");
+
+        commands.appendJobSummary(":wave: Hello from Quarkus GitHub Action");
+    }
+}
