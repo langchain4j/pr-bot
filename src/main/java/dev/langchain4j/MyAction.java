95f1506307ec06fbd63d57d65496a2dc9d353174
LangChain4j <info@langchain4j.dev>
2024-12-13 16:35:22 +0100

    playing around

--- src/main/java/dev/langchain4j/MyAction.java
+++ src/main/java/dev/langchain4j/MyAction.java
@@ -1,21 +1,11 @@
 package dev.langchain4j;
 
 import io.quarkiverse.githubaction.Action;
-import io.quarkiverse.githubapp.event.PullRequest;
-import org.kohsuke.github.GHEventPayload;
-import org.kohsuke.github.GHPullRequest;
-
-import java.io.IOException;
 
 public class MyAction {
 
     @Action
-    void action(@PullRequest GHEventPayload.PullRequest pullRequest) throws IOException {
-        GHPullRequest pr = pullRequest.getPullRequest();
+    void action() {
 
-        System.out.println("Repository: " + pr.getRepository().getFullName());
-        System.out.println("Issue title: " + pr.getTitle());
-
-        pr.comment("test comment");
     }
 }
